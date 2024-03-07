e708a4a806fd0a0a5e0b79ac1ca0164ac49eff08
MorgynKass <morgynkass@gmail.com>
2024-03-07 13:09:16 -0700

    finished S37D37

---
+++ Week-4/Habit-Tracker/main.py
@@ -0,0 +1,73 @@
+import requests
+from datetime import datetime
+
+USERNAME = "YOUR USERNAME"
+TOKEN = "YOUR SELF GENERATED TOKEN"
+GRAPH_ID = "YOUR GRAPH ID"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+NAME = "YOUR NAME"
+UNIT = "YOUR UNIT"
+TYPE = "YOUR TYPE"
+COLOR = "YOUR COLOR"
+
+DATA = "YOUR DATA"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+## POST
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": NAME,
+    "unit": UNIT,
+    "type": TYPE,
+    "color": COLOR
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": DATA,
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": DATA
+}
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
